e119cb1e2c4f80f3995a13d6fd79a0e58b9ab4eb
mzaksana <mzaksana@gmail.com>
2024-09-05 23:41:46 +0700

    :fire: remove

--- examples/simple.js
+++
@@ -1,8 +0,0 @@
-import globalRedisearch, { Redisearch } from "k6/x/redisearch";
-
-export default function () {
-  console.log(globalRedisearch.greeting);
-
-  let instance = new Redisearch("Wonderful World");
-  console.log(instance.greeting);
-}
